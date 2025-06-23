6c9656761fbac3d873a465d071eea36409a29dbd
Nicholas obonyo abala <nicholasobonyo444@gmail.com>
2025-06-23 17:33:01 +0300

    implement palidrome checking function with test and add sample calls

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const length = word.length;
+  for (let i = 0; i < length / 2; i++) {
+    if (word[i] !== word[length - 1 - i]) {
+      return false; // If characters don't match, it's not a palindrome
+    }
+  }
+  return true; // If all characters match, it is a palindrome
+  
 }
 
 /* 
@@ -23,3 +31,9 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+isPalindrome("racecar"); // true
+isPalindrome("robot"); // false
+isPalindrome("abba"); // true
+isPalindrome("a"); // false
+isPalindrome("ab"); // false
